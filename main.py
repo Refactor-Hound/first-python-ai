ac43b056122a06ee5209c1ee43b1d0bfdd1401e8
Refactor-Hound <peterjayshepherd@gmail.com>
2025-11-05 10:57:29 -0500

    spelling, formatting, and corrections.

--- main.py
+++ main.py
@@ -11,15 +11,26 @@ client = genai.Client(api_key=api_key)
 
 
 def main():
-  if len(sys.argv) > 1:
-    user_prompt = sys.argv[1]
-  else: 
-    print("Prompt arguement required.")
-    sys.exit(1)
+  
 
-  messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
-  resp =client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
-  return print(f"{resp.text}\nPrompt tokens: {resp.usage_metadata.prompt_token_count}\nResponse tokens: {resp.usage_metadata.candidates_token_count}")
+  
+  
+  if len(sys.argv) < 2: 
+    print("Prompt argument required.")
+    sys.exit(1)
+  else:
+    user_prompt = sys.argv[1]
+    verbose = "--verbose" in sys.argv[2:]
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
+    resp =client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+    if verbose:
+      return print(f"User prompt: {user_prompt}\nPrompt tokens: {resp.usage_metadata.prompt_token_count}\nResponse tokens: {resp.usage_metadata.candidates_token_count}")
+    else:
+      return print(resp.text)
+
+
+
+  # return print(f"{resp.text}\nPrompt tokens: {resp.usage_metadata.prompt_token_count}\nResponse tokens: {resp.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
