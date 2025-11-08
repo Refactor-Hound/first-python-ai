4a42078f37bc31dd0a990fde7fea30c3d29f3e15
Refactor-Hound <peterjayshepherd@gmail.com>
2025-11-07 21:32:51 -0500

    write_file.py and tests

---
+++ functions/write_file.py
@@ -0,0 +1,22 @@
+import os
+
+def write_file(working_directory, file_path, content):
+
+    abs_base = os.path.abspath(working_directory)
+    abs_target = os.path.abspath(os.path.join(abs_base, file_path))
+
+    base_with_sep = abs_base if abs_base.endswith(os.sep) else abs_base + os.sep
+    if not abs_target.startswith(base_with_sep):
+        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+
+    try:
+        parent_dir = os.path.dirname(abs_target)
+        if parent_dir and not os.path.exists(parent_dir):
+            os.makedirs(parent_dir)
+
+        with open(abs_target, "w") as f:
+            f.write(content)
+
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as e:
+        return f"Error: {e}"
--- tests.py
+++ tests.py
@@ -40,20 +40,24 @@
 
 
 # JUST TO PASS THE SPECIFIC COURSE REQUIREMENTS
-from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 def demo():
     # print("Result for lorem:")
     # print(get_file_content("calculator", "lorem.txt"))
 
-    print("Result for main.py:")
-    print(get_file_content("calculator", "main.py"))
-    print("Result for 'pkg/calculator.py' directory:")
-    print(get_file_content("calculator", "pkg/calculator.py"))
-    print("Result for '/bin/cat' directory:")
-    print(get_file_content("calculator", "/bin/cat"))
-    print("Result for non-exist file:")
-    print(get_file_content("calculator", "pkg/this_does_not_exist.py"))
+    # print("Result for main.py:")
+    # print(get_file_content("calculator", "main.py"))
+    # print("Result for 'pkg/calculator.py' directory:")
+    # print(get_file_content("calculator", "pkg/calculator.py"))
+    # print("Result for '/bin/cat' directory:")
+    # print(get_file_content("calculator", "/bin/cat"))
+    # print("Result for non-exist file:")
+    # print(get_file_content("calculator", "pkg/this_does_not_exist.py"))
+
+    print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+    print(write_file("calculator", "pkg/morelorem.txt", "lorel upsum dolor sit amet"))
+    print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
 
 if __name__ == "__main__":
     demo()
