f6ebdaa0eeacf94c053cdca33a8e0d88c86f1d12
Refactor-Hound <peterjayshepherd@gmail.com>
2025-11-11 13:01:20 -0500

    Adding system config to main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from config import *
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -22,7 +23,7 @@ def main():
     user_prompt = sys.argv[1]
     verbose = "--verbose" in sys.argv[2:]
     messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
-    resp =client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+    resp =client.models.generate_content(model="gemini-2.0-flash-001", contents=messages, config=types.GenerateContentConfig(system_instruction=system_prompt))
     if verbose:
       return print(f"User prompt: {user_prompt}\nPrompt tokens: {resp.usage_metadata.prompt_token_count}\nResponse tokens: {resp.usage_metadata.candidates_token_count}")
     else:
